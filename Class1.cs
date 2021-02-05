2a45d281d52c5b8ad7c359c03216373bdb902f62
manasa <54406519+manasa423@users.noreply.github.com>
2021-02-05 15:53:46 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+﻿//perfect number
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Class1
+{
+    public static void Main()
+    {
+        int n=28, i, sum;
+
+      
+
+        sum = 0;
+        Console.Write("The positive divisor  : ");
+        for (i = 1; i < n; i++)
+        {
+            if (n % i == 0)
+            {
+                sum = sum + i;
+                Console.Write("{0}  ", i);
+            }
+        }
+        Console.Write("\nThe sum of the divisor is : {0}", sum);
+        if (sum == n)
+            Console.Write("\nSo, the number is perfect.");
+        else
+            Console.Write("\nSo, the number is not perfect.");
+        Console.Write("\n");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//strong or not
+
+using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=371, r, sum = 0, temp;
+           
+            temp = n;
+            while (n > 0)
+            {
+                r = n % 10;
+                sum = sum + (r * r * r);
+                n = n / 10;
+            }
+            if (temp == sum)
+                Console.Write("Armstrong Number.");
+            else
+                Console.Write("Not Armstrong Number.");
+        }
+    }
+}
